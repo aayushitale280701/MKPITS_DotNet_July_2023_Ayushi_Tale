7d732fac8547b57cc40e81cdb81b3e3dd13bccae
aayushitale280701 <ayushitale2020@gmail.com>
2023-10-12 15:54:46 +0530

    accept_character_user_csharp.cs

---
+++ csharp/accept_character_user_csharp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace acceptcharacteruser
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            char ch;
+            Console.WriteLine("Enter the character");
+            ch=Convert.ToChar(Console.Read());
+            Console.WriteLine("ch = " +ch);
+            Console.ReadKey();
+        }
+    }
+}
