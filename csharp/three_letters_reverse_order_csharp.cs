00a8b10c876619f4d0b15925fa4259e9ec6a2610
aayushitale280701 <ayushitale2020@gmail.com>
2023-10-12 18:29:33 +0530

    three_letters_reverse_order_csharp.cs

---
+++ csharp/three_letters_reverse_order_csharp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace threelettersreverseorder
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            char  c1, c2, c3;
+            Console.WriteLine("Enter char: a");
+            c1 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter char: b");
+            c2 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter char: t");
+            c3 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(" " +c3);
+            Console.WriteLine(" " +c2);
+            Console.WriteLine(" " +c1);
+
+        }
+    }
+}
