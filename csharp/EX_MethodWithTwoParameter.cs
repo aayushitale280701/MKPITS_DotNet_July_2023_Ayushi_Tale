4b9f3841f7b738d9ac5e0b1dc7400d065de932bf
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-05 22:06:26 +0530

    EX_MethodWithTwoParameter.cs         MethodOverloaded.cs         classStudent_Overload MethodFivePara.cs

---
+++ csharp/EX_MethodWithTwoParameter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class product
+    {
+        int productid;
+        string productname;
+        public void getproductdata(string productname)
+        {
+            productid = 1;
+            this.productname = productname;
+        }
+        public void getproductdata(int productid, string productname)
+        {
+            this.productid = productid;
+            this.productname = productname;
+        }
+        public void getproductdata(string productname, int productid)
+        {
+            this.productid = productid;
+            this.productname = productname;
+        }
+        public void display()
+        {
+            Console.WriteLine("Productid " + productid);
+            Console.WriteLine("Productname " + productname);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            product p = new product();
+            p.getproductdata("Mouse");
+            p.display();
+            Console.WriteLine("Method with 2 parameters integer first");
+            p.getproductdata(123, "monitor");
+            p.display();
+            Console.WriteLine("Method with 2 parameters string first");
+            p.getproductdata("Monitor", 222);
+            p.display();
+
+
+        }
+    }
+}
---
+++ csharp/classStudent_Overload MethodFivePara.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace classStd
+{
+    class Student
+    {
+        int student_rollno;
+        string student_name;
+        string student_course;
+        int student_fee;
+
+        public void getdata()
+        {
+            student_rollno = 11;
+            student_name = "Ayushi";
+            student_course = "DotNet";
+            student_fee = 20000;
+
+        }
+        //method overloaded
+        public void getdata(int  student_rollno)
+        {
+            this.student_name = student_name;
+        }
+        public void getdata(int student_rollno, string student_name)
+        {
+            this.student_name = student_name;
+            this.student_rollno= student_rollno;
+        }
+        
+        public void getdata(int student_rollno, string student_name, string  student_course)
+        {
+            this.student_rollno = student_rollno;
+            this.student_name = student_name;
+            this.student_course = student_course;
+        }
+        public void getdata(int student_rollno, string student_name, string student_course, int student_fee)
+        {
+            this.student_rollno=student_rollno;
+            this.student_name = student_name;
+            this.student_course=student_course;
+            this.student_fee=student_fee;
+        }
+        public void display()
+        {
+            Console.WriteLine("Student RollNo= " + student_rollno);
+            Console.WriteLine("Student Name= " + student_name);
+            Console.WriteLine("Student Course= " + student_course);
+            Console.WriteLine("Student Fee= " + student_fee);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.getdata();
+            std.display();
+            Console.WriteLine("Student Details\n");
+            Student std1=new Student();
+            std1.getdata(22,"Om");
+            std1.display();
+            Console.WriteLine("Student1 Details\n");
+            Student std2 =new Student();
+            std2.getdata(22,"Om", "DotNet");
+            std2.display();
+            Console.WriteLine("Student2 Details\n");
+            Student std3=new Student();
+            std3.getdata(22, "Om", "DotNet", 50000);
+            std3.display();
+            Console.ReadKey();
+        }
+    }
+}
