5efb2da5efcb1926a16fa73352771e1c970ce91c
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-06 22:32:28 +0530

    EX_StaticField_DifferentObjects.cs         StaticMethod_StaticConstructor.cs

---
+++ csharp/EX_StaticField_DifferentObjects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace StaticField_DiffObjects
+{
+    class book
+    {
+        static string title;
+
+        static book()
+        {
+            Console.WriteLine("Satic Constuctor Called");
+            title = "MKPITS";
+        }
+        public void display()
+        {
+            Console.WriteLine("Title = " + title);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book b1= new book();
+            b1.display();//this will call static constructor only once
+            book b2= new book();
+            b2.display();// now static constructor will not be called;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp/StaticMethod_StaticConstructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace staticMethodAndConstructor
+{
+    class Book
+    {
+        static string title;
+        static Book()
+        {
+            Console.WriteLine("Static Constructor Called");
+            title = "MKPITS";
+        }
+        public static void dispaly()
+        {
+            Console.WriteLine("Title = " +  title);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book.dispaly();
+            Console.ReadKey();
+        }
+    }
+}
