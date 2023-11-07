80bc1601d59a2aa140c12619636b6dc07a679532
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-07 17:00:23 +0530

    Ex_constructorDestructor.cs         MthodOverloading_diffTypeParameter.cs

---
+++ csharp/Ex_constructorDestructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ExConstructorDistructor
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Console.WriteLine("Constuctor Called");
+        }
+        ~Employee()
+        {
+            Console.WriteLine("Distructor Called");
+        }
+    }
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            Console.ReadKey();
+            Employee e2 = new Employee();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp/MthodOverloading_diffTypeParameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace typeofdiffpara
+{
+    class Calculate
+    {
+        public void addition(int n1,int n2)
+        {
+            int res = n1 + n2;
+            Console.WriteLine("Addition of 2 int is : "+res);
+        }
+        public void addition (float n1,float n2)
+        {
+            float res = n1 + n2;
+            Console.WriteLine("Addition of 2 float is : " + res);
+        }
+        class program
+        {
+            public static void Main(string[] args)
+            {
+                Calculate c = new Calculate();
+                c.addition(1, 2);
+                Console.WriteLine("--------Now calling adding float is------- ");
+                c.addition(2.2f,3.2f);
+                Console.ReadKey();
+            }
+        }
+    }
+}
