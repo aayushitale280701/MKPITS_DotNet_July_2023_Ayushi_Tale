1610e14c666fd34866595e0b4a96c956fd9d4cf8
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-11 23:14:41 +0530

    ClassCar_ParameterizedConstructorOverloading.cs         ClassCar_contorWithNoParaMethodDisplay.cs

---
+++ csharp/ClassCar_ParameterizedConstructorOverloading.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CLASSCAR
+{
+    class Car
+    {
+        int id;
+        string name;
+        int price;
+
+        //Creating a constructor with no parameters
+        public Car()
+        {
+            Console.WriteLine("Constructor with no parameter called");
+            id = 546;
+            name = "KIW";
+            price = 2000000;
+        }
+        //creating a  parameterized constructor
+        public Car (int id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+        public void dispaly()
+        {
+            Console.WriteLine("Car ID- " + id);
+            Console.WriteLine("Car Name- " + name);
+            Console.WriteLine("Car Price- "+ price);
+        }
+    }
+    class program
+    {
+        public static void Main (string[] args)
+        {
+            Car c=new Car();
+            c.dispaly(); //this will call constructor with no parameter automatically
+            Console.WriteLine("\n");
+            Car c1 = new Car(546,"Swite", 1500000); //this will call parameterized constructor
+            c1.dispaly();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp/ClassCar_contorWithNoParaMethodDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ClassCar
+{
+    class Car
+    {
+        int carid;
+        string name;
+        int price;
+        //creating a constructor with no parameters
+        public Car()
+        {
+            carid = 333;
+            name = "BMW";
+            price = 60000;
+        }
+        public void display()
+        {
+            Console.WriteLine("Car ID = " +  carid);
+            Console.WriteLine("Car Name = " +  name);
+            Console.WriteLine("Car Price = " + price);
+        }
+    }
+    class program
+    {
+        public static void Main (string[] args)
+        {
+            Car c = new Car();//this will call constructor automaticaly
+            c.display();
+            Console.ReadLine();
+        }
+    }
+}
