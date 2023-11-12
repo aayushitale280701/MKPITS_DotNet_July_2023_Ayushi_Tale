64451bfd6994d0bbd131e5d6095d0c81f88198a0
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-12 23:14:28 +0530

    EX_StaticMemberStaticMethodStaticConstructor.cs

---
+++ csharp/EX_StaticMemberStaticMethodStaticConstructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace MemeberMethodConstructor
+{
+    class Car
+    {
+        static string name;
+        static int id;
+        static int price;
+        static Car()
+        {
+            Console.WriteLine("Static Constructor Called");
+            name = "BMW";
+            id = 5678;
+            price = 6000000;
+        }
+        public static void dispaly()
+        {
+            Console.WriteLine("Model Name:- " +name);
+            Console.WriteLine("Model ID:- " + id);
+            Console.WriteLine("Model Price:- " + price);
+        }
+    }
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Car.dispaly();
+            Console.ReadKey();
+        }
+    }
+}
