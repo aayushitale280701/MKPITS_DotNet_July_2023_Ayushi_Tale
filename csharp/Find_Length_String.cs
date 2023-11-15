48ecca41dbce964058202f94c1c8c6c0ded50a20
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-16 00:03:39 +0530

    Find_Length_String.cs

---
+++ csharp/Find_Length_String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace lengthString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int l = 0;
+            Console.WriteLine("Enter the String");
+            str= Console.ReadLine();
+            foreach(char chr in str)
+            {
+                l += 1;
+            }
+            Console.WriteLine("Length of string is :- {0} \n", l);
+            Console.ReadKey();
+        }
+    }
+}
