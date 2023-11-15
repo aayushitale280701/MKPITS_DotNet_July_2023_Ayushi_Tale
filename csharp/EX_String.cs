099bda327e323ec719c6772dd4b43378d26cb156
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-15 23:51:49 +0530

    EX_String.cs

---
+++ csharp/EX_String.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EXstring
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter The String:- ");
+            str= Console.ReadLine();
+            Console.WriteLine("The string you entered is : {0}\n",str);
+        }
+    }
+}
+    
