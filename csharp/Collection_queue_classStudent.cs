6130ad3e7d83059eb4882a86023c3583cba8f5d1
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-25 23:06:48 +0530

    Collection_queue_classStudent.cs         queue_usingInteger_ex.cs

---
+++ csharp/Collection_queue_classStudent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+namespace collectionQueue
+{
+    class Student
+    {
+        public int rno;
+        public string name;
+        public Student(int rno, string name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Student s1 = new Student(1, "Ayushi");
+                Student s2 = new Student(2, "Om");
+                Student s3 = new Student(3, "Ritu");
+                Queue s = new Queue();
+                s.Enqueue(s1);
+                s.Enqueue(s2);
+                s.Enqueue(s3);
+                foreach(Student val in  s)
+                {
+                    Console.WriteLine("Roll No: "+val.rno);
+                    Console.WriteLine("Name: "+val.name+"\n");
+                }
+                Student r = (Student)s.Dequeue();
+                Console.WriteLine("Item Deque: "+r.rno+" : "+r.name);
+                Console.WriteLine("After Deque: ");
+                foreach(Student val in s)
+                {
+                    Console.WriteLine("Name: " + val.name);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ csharp/queue_usingInteger_ex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace usingInteger
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine("Adding some No   ");
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            Console.WriteLine("\n");
+            Console.WriteLine("Capacity: {0} ", al.Capacity);
+            Console.WriteLine("Count: {0} ", al.Count);
+            Console.WriteLine("\n");
+            Console.WriteLine("Content  ");
+            foreach(int i in al) Console.WriteLine("  "+i);
+            Console.WriteLine("\n");
+            Console.WriteLine("Stored in Contened  ");
+            al.Sort();
+            foreach(int i in al) Console.WriteLine("  "+i);
+            Console.WriteLine("\n");
+            Console.ReadLine();
+        }
+    }
+}
