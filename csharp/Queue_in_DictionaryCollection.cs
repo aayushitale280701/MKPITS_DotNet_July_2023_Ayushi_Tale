2c7f0c239b9a9ee6f6c5634f2fcf56bbcda55d1d
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-28 22:43:43 +0530

    Queue_in_DictionaryCollection.cs         Stacks_in_DictionaryCollection.cs

---
+++ csharp/Queue_in_DictionaryCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace DictionaryCollection_Ex
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            Queue<string> msgs = new Queue<string>();
+            msgs.Enqueue("Message 1");
+            msgs.Enqueue("Message 2");
+            msgs.Enqueue("Message 3");
+            msgs.Enqueue("Message 4");
+            msgs.Enqueue("Message 5");
+            msgs.Enqueue("Message 6");
+            Console.WriteLine(msgs.Dequeue());
+            Console.WriteLine(msgs.Peek());
+            Console.WriteLine(msgs.Peek());
+            Console.WriteLine("\n");
+            foreach(string msg in msgs)
+            {
+                Console.WriteLine(msg);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp/Stacks_in_DictionaryCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace DictionaryCollection_Ex
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Stack<int>stc = new Stack<int>();
+            stc.Push(1);
+            stc.Push(7);
+            stc.Push(3);
+            stc.Push(9);
+            stc.Push(5);
+            stc.Push(6);
+            stc.Push(2);
+            Console.WriteLine(stc.Pop());
+            Console.WriteLine(stc.Peek());
+            Console.WriteLine(stc.Peek());
+            Console.WriteLine("\n");
+            foreach (int i in stc)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
