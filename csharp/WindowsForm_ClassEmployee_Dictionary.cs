c295a79efec038e6040ad002c9e6a9fd63bfc478
aayushitale280701 <ayushitale2020@gmail.com>
2023-11-29 18:18:23 +0530

    WindowsForm_ClassEmployee_Dictionary.cs         WindowsFormsApp_ClassCustomer_UsingProperty.cs

---
+++ csharp/WindowsForm_ClassEmployee_Dictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm_ClassEmployee_Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, Employee> d = new Dictionary <int, Employee>();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Employee emp1 = new Employee(Convert.ToInt32(textBox1.Text), textBox2.Text);
+            Employee emp2 = new Employee(Convert.ToInt32(textBox3.Text), textBox4.Text);
+            d.Add(emp1.empid, emp1);
+            d.Add(emp2.empid, emp2);
+        }   
+
+         
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<int, Employee> kv in d)
+            {
+                sb.Append("Emp id " + kv.Key + "Emp name " + kv.Value.empname + "\n");
+            }
+            label5.Text = sb.ToString();
+        }
+    }
+}
+
+
+/*internal class Employee
+{
+
+
+    public int empid { get; set; }
+    public string empname { get; set; }
+    public Employee(int e1, string en)
+    {
+        empid = e1;
+        empname = en;
+    }
+}
+*/
---
+++ csharp/WindowsFormsApp_ClassCustomer_UsingProperty.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ClassCustomer_UsingProperty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int,Customer>cust=new Dictionary<int, Customer>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer c1 = new Customer (Convert.ToInt32(textBox1.Text), textBox2.Text);
+            Customer c2 = new Customer (Convert.ToInt32(textBox3.Text), textBox4.Text);
+            cust.Add(c1.id, c1);
+            cust.Add(c2.id, c2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(KeyValuePair<int,Customer> kv in cust)
+            {
+                sb.Append("Cust id: " + kv.Key + "Customer name: " + kv.Value.name + "\n");
+            }
+            label5.Text = sb.ToString();
+        }
+    }
+}
+
+/*
+ * internal class Customer
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public Customer (int i, string n)
+        {
+            id = i;
+           name = n;
+        }
+    }
+*/
+
