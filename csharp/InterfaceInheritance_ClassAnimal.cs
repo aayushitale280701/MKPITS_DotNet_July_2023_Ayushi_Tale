1e52a58fe7bd8c8dd69733ae5572557f78482be8
aayushitale280701 <ayushitale2020@gmail.com>
2023-12-03 18:30:58 +0530

    InterfaceInheritance_ClassAnimal.cs         InterfaceInheritance_ClassDrawable.cs

---
+++ csharp/InterfaceInheritance_ClassAnimal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace ClassAnimalInterfaceInheritance
+{
+    interface Animals
+    {
+        void Makesound();
+
+    }
+    class Dog : Animals
+    {
+        public void Makesound()
+        {
+            Console.WriteLine("Dog Sound is Bhooo....");
+        }
+    }
+    class Cat : Animals
+    {
+        public void Makesound()
+        {
+            Console.WriteLine("Cat sound is mew mew....");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals[] d = new Animals();
+            d[0]=new Dog() ;
+            d[1]=new Cat() ;
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i].Makesound();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/InterfaceInheritance_ClassDrawable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace InterfaceInheritance
+{
+    interface Drawable
+    {
+        void Draw();
+    }
+    class Circle : Drawable
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Circle Drawing...  ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drawable d = new Circle();
+            d.Draw();
+            Console.ReadKey();
+        }
+    }
+}
