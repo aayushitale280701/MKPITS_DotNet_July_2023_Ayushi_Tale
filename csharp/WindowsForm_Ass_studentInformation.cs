f47919c9bc7d94a5ec5dfc18275564bb6f3190b4
aayushitale280701 <ayushitale2020@gmail.com>
2023-12-05 22:14:24 +0530

    WindowsForm_Ass_studentInformation.cs         WindowsForm_ClaculateAgeDate.cs         WindowsForm_CreatedRegistrationForm.cs         WindowsForm_DateTimeControl.cs         WindowsForm_HyperlinkControl.cs         WindowsForm_MaskedTextbox.cs         WindowsForm_MonthCalendar_NumericupdownControl.cs         WindowsForm_PictureBoxControl.cs         WindowsForm_ProgressBarControl.cs         WindowsForm_SelectDate.cs

---
+++ csharp/WindowsForm_Ass_studentInformation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm_Ass_studentInformation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome....!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            string name = textBox1.Text;
+            string age = numericUpDown1.Text;
+            if(textBox1.Text == " ")
+            {
+                MessageBox.Show("Please Enter Your Name and Age... ");
+            }
+            if(numericUpDown1.Value <= 18)
+            {
+                MessageBox.Show("Please Enter Age Greater-than 18... ");
+            }
+            else
+            {
+                sb.Append("Name: " + textBox1.Text + "\n");
+                sb.Append("Age:  " + numericUpDown1.Value.ToString() + "\n");        
+            }
+            label3.Text = sb.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            numericUpDown1.Value = 0;
+            
+        }
+    }
+}
---
+++ csharp/WindowsForm_DateTimeControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm_DateTimeControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dateTimePicker1.Text);
+            label3.Text = dateTimePicker1.Text;
+            label4.Text = dateTimePicker1.Value.ToLongDateString();
+            label5.Text = dateTimePicker1.Value.ToShortDateString();
+            label6.Text = dateTimePicker1.Value.Date.Year.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
