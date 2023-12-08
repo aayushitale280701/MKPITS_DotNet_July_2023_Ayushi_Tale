f37125558c99c54cb70ea8fc313b27f5cf430dd6
aayushitale280701 <ayushitale2020@gmail.com>
2023-12-08 23:02:22 +0530

    LinkedList_Number_Adds.cs         Queues_string.cs

---
+++ csharp/LinkedList_Number_Adds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> Num = new LinkedList<int>();
+            Num.AddLast(45);
+            Num.AddLast(55);
+            Num.AddLast(75);
+            Num.AddLast(65);
+            Num.AddLast(35);           
+
+            foreach(int num in Num)
+            {
+                Console.WriteLine(Num);
+            }
+        }
+    }
+}
---
+++ csharp/Queues_string.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Queues_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> Msg = new Queue<string>();
+            Msg.Enqueue("Message 1");
+            Msg.Enqueue("Message 2");
+            Msg.Enqueue("Message 3");
+            Msg.Enqueue("Message 4");
+            Console.WriteLine(Msg.Dequeue());
+            Console.WriteLine(Msg.Peek());
+            Console.WriteLine(Msg.Peek());
+
+            Console.WriteLine("\n");
+
+            foreach(string M in Msg)
+            {
+                Console.WriteLine(Msg);
+            }
+        }
+    }
+}
