1de49e901afdae4b640f518bccb64ea9def31a87
aayushitale280701 <ayushitale2020@gmail.com>
2023-12-11 22:14:56 +0530

    Animal_Machine_OOPS.cs         BankManegment_OOPS_MachineTest.cs         LibraryManagment_Machine_OOPS.cs

---
+++ csharp/Animal_Machine_OOPS.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic animal sound");
+    }
+}
+
+class Mammal : Animal
+{
+    public Mammal(string name, int age) : base(name, age)
+    {
+    }
+
+}
+
+class Reptile : Animal
+{
+    public Reptile(string name, int age) : base(name, age)
+    {
+    }
+
+}
+
+class Bird : Animal
+{
+    public Bird(string name, int age) : base(name, age)
+    {
+    }
+
+}
+
+class Lion : Mammal
+{
+    public Lion(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Roar!");
+    }
+
+}
+
+class Snake : Reptile
+{
+    public Snake(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Hiss!");
+    }
+
+}
+
+class Eagle : Bird
+{
+    public Eagle(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Screech!");
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Lion lion = new Lion("Simba", 5);
+        Snake snake = new Snake("Nagini", 3);
+        Eagle eagle = new Eagle("Thunder", 2);
+
+        Animal[] animals = { lion, snake, eagle };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"Name: {animal.Name}, Age: {animal.Age}");
+            animal.MakeSound();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ csharp/LibraryManagment_Machine_OOPS.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Author
+{
+    public string AuthorName { get; }
+
+    public Author(string authorName)
+    {
+        AuthorName = authorName;
+    }
+}
+
+class Book
+{
+    public string Title { get; }
+    public string ISBN { get; }
+    public Author Author { get; }
+
+    public Book(string title, string isbn, Author author)
+    {
+        Title = title;
+        ISBN = isbn;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {Title}, ISBN: {ISBN}, Author: {Author.AuthorName}";
+    }
+}
+
+class Library
+{
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+        Console.WriteLine($"Book added to the library: {book}");
+    }
+
+    public void RemoveBook(string isbn)
+    {
+        Book bookToRemove = null;
+
+        foreach (var book in books)
+        {
+            if (book.ISBN == isbn)
+            {
+                bookToRemove = book;
+                break;
+            }
+        }
+
+        if (bookToRemove != null)
+        {
+            books.Remove(bookToRemove);
+            Console.WriteLine($"Book removed from the library: {bookToRemove}");
+        }
+        else
+        {
+            Console.WriteLine($"Book with ISBN {isbn} not found in the library.");
+        }
+    }
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine("Books in the library:");
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Author author1 = new Author("John Doe");
+        Author author2 = new Author("Jane Smith");
+
+        Book book1 = new Book("Introduction to C#", "123456", author1);
+        Book book2 = new Book("Introduction to C#", "789012", author2);
+        Book book3 = new Book("Advanced C#", "345678", author1);
+
+        Library library = new Library();
+
+        library.AddBook(book1);
+        library.AddBook(book2);
+        library.AddBook(book3);
+
+        library.DisplayBooks();
+
+        library.RemoveBook("789012");
+
+        library.DisplayBooks();
+
+        Console.ReadKey();
+    }
+}
