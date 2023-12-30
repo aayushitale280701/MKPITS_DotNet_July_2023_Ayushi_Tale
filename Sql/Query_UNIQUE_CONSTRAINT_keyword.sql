7ece351e32f91e70e55142583e1c10af0ddc6b99
aayushitale280701 <ayushitale2020@gmail.com>
2023-12-30 18:12:55 +0530

    Query_UNIQUE_CONSTRAINT_keyword.sql

--- Sql/Query_UNIQUE_CONSTRAINT_keyword.sql
+++ Sql/Query_UNIQUE_CONSTRAINT_keyword.sql
@@ -1,14 +1,13 @@
-CREATE TABLE Person5 (PersonID INT IDENTITY PRIMARY KEY ,
+CREATE TABLE Person55 (PersonID INT IDENTITY PRIMARY KEY ,
                       FirstName varchar (255),
 					  LastName varchar (255),
 					  Email varchar (255),
 					  CONSTRAINT Unique_Email UNIQUE(Email)
 					  );
-INSERT INTO Person5 VALUES ('Ayushi' , 'Tale', 'ayu@123')
-INSERT INTO Person5 VALUES ('Ayush' , 'kade', 'ayush@123')
-INSERT INTO Person5 VALUES ('Om' , 'Tale', 'Om@123')
-INSERT INTO Person5 VALUES ('Rajesh' , 'Tale', 'raju@123')
-INSERT INTO Person5 VALUES ('Ritu' , 'Tale', 'ritu@123')
+INSERT INTO Person55 VALUES ('Ayushi' , 'Tale', 'ayu@123')
+INSERT INTO Person55 VALUES ('Ayush' , 'kade', 'ayush@123')
+INSERT INTO Person55 VALUES ('Om' , 'Tale', 'Om@123')
+INSERT INTO Person55 VALUES ('Rajesh' , 'Tale', 'raju@123')
+INSERT INTO Person55 VALUES ('Ritu' , 'Tale', 'ritu@123')
 
-SELECT * FROM Person5
-
+SELECT * FROM Person55
