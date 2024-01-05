8111ab1468aadf12593f26168fdf03990c1dfe7d
aayushitale280701 <ayushitale2020@gmail.com>
2024-01-05 17:24:42 +0530

    Query_DB_MkpSample.sql

---
+++ Sql/Query_DB_MkpSample.sql
@@ -0,0 +1,11 @@
+Create table Employee (Emp_ID int primary key,
+                       Emp_Name varchar(50),
+					   Dob varchar(50),
+					   Age varchar(50),
+					   City varchar(50),
+					   Mobile_No varchar(10),
+					   Salary varchar(10)
+					   )
+insert into Employee values (111,'Ayushi','28/07/2001','22','Nagpur','9876543210','45000')
+insert into Employee values (112,'Prachi','12/10/2000','23','Nagpur','741829630','25000')
+select * from Employee
