fbd1a6cc91ddb1a2f355458333ddd9dfb5658d75
aayushitale280701 <ayushitale2020@gmail.com>
2024-01-13 14:44:21 +0530

    WindowsForm_InstituteCourseRegistration_DB.cs         WindowsForms_Fendal_Product.cs

---
+++ AdoDotnet/WindowsForm_InstituteCourseRegistration_DB.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm_InstituteCourseRegistration_DB
+{
+    public partial class Form1 : Form
+    {
+        string strcon = "server=.\\sqlexpress;integrated security=true;database=Nation";
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Text = DateTime.Now.ToShortDateString();
+            getnation();
+            getstate();
+            getcity();
+            if (radioButton1.Checked)
+                textBox2.Text = "1000";
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            getstate();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            getcity();
+        }
+
+        public void getnation()
+        {
+            try
+            {
+                con = new SqlConnection(strcon);
+                da = new SqlDataAdapter("select * from TableNation order by NationId ", con);
+                ds = new DataSet();
+                da.Fill(ds, "TableNation");
+                comboBox1.DataSource = ds.Tables["TableNation"];
+                comboBox1.ValueMember = "NationId";
+                comboBox1.DisplayMember = "NationName";
+
+            }
+            catch (SqlException ee)
+            {
+                MessageBox.Show(ee.ToString());
+            }
+        }
+
+        public void getstate()
+        {
+            if (comboBox1.SelectedValue.ToString() != "System.Data.DataRowView")
+            {
+
+                //   MessageBox.Show("nation id " + comboBox1.SelectedValue.ToString());
+                try
+                {
+
+                    con = new SqlConnection(strcon);
+                    da = new SqlDataAdapter("select * from TableState  where NationId=@NationId", con);
+                    da.SelectCommand.Parameters.AddWithValue("@NationID", comboBox1.SelectedValue);
+                    ds = new DataSet();
+                    ds.Clear();
+                    da.Fill(ds, "TableState");
+                    comboBox2.DataSource = ds.Tables["TableState"];
+                    comboBox2.ValueMember = "StateId";
+                    comboBox2.DisplayMember = "StateName";
+
+                }
+                catch (SqlException ee)
+                {
+                    MessageBox.Show(ee.ToString());
+                }
+            }
+        }
+
+        public void getcity()
+        {
+            if (comboBox2.SelectedValue.ToString() != "System.Data.DataRowView")
+            {
+
+                //   MessageBox.Show("nation id " + comboBox1.SelectedValue.ToString());
+                try
+                {
+
+                    con = new SqlConnection(strcon);
+                    da = new SqlDataAdapter("select * from TableCity  where StateId=@StateId", con);
+                    da.SelectCommand.Parameters.AddWithValue("@StateID", comboBox2.SelectedValue);
+                    ds = new DataSet();
+                    ds.Clear();
+                    da.Fill(ds, "TableCity");
+                    comboBox3.DataSource = ds.Tables["TableCity"];
+                    comboBox3.ValueMember = "CityId";
+                    comboBox3.DisplayMember = "CityName";
+
+                }
+                catch (SqlException ee)
+                {
+                    MessageBox.Show(ee.ToString());
+                }
+            }
+        }
+
+        public enum category { student, it_professional }
+        public enum Gender  {Male,Female,Other}
+
+        category cat;
+        Gender ge;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the selected payment date from the DateTimePicker
+            DateTime selectedDate = dateTimePicker1.Value;
+
+            // Validate that the selected date is not earlier than today
+            if (selectedDate.Date < DateTime.Today)
+            {
+                MessageBox.Show("Please select a payment date that is on or after today's date.", "Invalid Date Selection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Full name cant be blank");
+            }
+            else
+            {
+                //for category
+                if (radioButton1.Checked)
+                {
+                    cat = category.student;
+                }
+                else if (radioButton2.Checked)
+                {
+                    cat = category.it_professional;
+                }
+                MessageBox.Show("Category " + cat);
+                //for gender
+                 if(radioButton3.Checked)
+                {
+                    ge = Gender.Male;
+                }
+                 else if (radioButton4.Checked)
+                {
+                    ge = Gender.Female;
+                }
+                else if (radioButton5.Checked)
+                {
+                    ge = Gender.Other;
+                }
+
+                MessageBox.Show("gender selected " + ge);
+                //code to save record in courseregdetail table
+                savecourseregdetail(cat, ge);
+            }
+        }
+
+        public void savecourseregdetail(category c, Gender g)
+        {
+            try
+            {
+                string str = "insert into TableCourseRegDetail values(@CategoryId,@FullName,@GenderID)";
+                SqlCommand command = new SqlCommand(str, con);
+                command.Parameters.AddWithValue("@CategoryID", Convert.ToInt32(c));
+                command.Parameters.AddWithValue("@FullName", textBox1.Text);
+                command.Parameters.AddWithValue("@GenderID", Convert.ToInt32(g));
+                con.Open();
+                command.ExecuteNonQuery();
+                MessageBox.Show("Record saved");
+                con.Close();
+                insertRegAddress();
+
+            }
+            catch (SqlException ee)
+            {
+                MessageBox.Show(ee.ToString());
+            }
+
+        }
+        public void insertRegAddress()
+        {
+            con.Open();
+            string str1 = "select max(CourseRegID) from TableCourseRegDetail";
+            SqlCommand cmd1 = new SqlCommand(str1, con);
+            int mcid = Convert.ToInt32(cmd1.ExecuteScalar());
+            // MessageBox.Show(mcid.ToString());
+            string str2 = "insert into TableRegAddress values(@CourseRegId,@NationId,@StateId,@CityId)";
+            SqlCommand cmd2 = new SqlCommand(str2, con);
+            cmd2.Parameters.AddWithValue("@CourseRegId", mcid);
+            cmd2.Parameters.AddWithValue("@NationId", comboBox1.SelectedValue);
+            cmd2.Parameters.AddWithValue("@StateId", comboBox2.SelectedValue);
+            cmd2.Parameters.AddWithValue("@CityId", comboBox3.SelectedValue);
+            cmd2.ExecuteNonQuery();
+           
+            MessageBox.Show("Stored TableRegAddress");
+            con.Close();
+            insertFeeDetail();
+        }
+
+        public void insertFeeDetail()
+        {
+            con.Open();
+            string str = "select max(CourseRegID) from TableCourseRegDetail";
+            SqlCommand cmd11 = new SqlCommand(str, con);
+            int mcid = Convert.ToInt32(cmd11.ExecuteScalar());
+            string s2 = "insert into TableFeeDetail values(@CourseRegId,@TotalAmount,@MiniPer,@PaidAmount,@BalAmount,@PaidDate)";
+            SqlCommand cmd1 = new SqlCommand(s2, con);
+            cmd1.Parameters.AddWithValue("@CourseRegID", mcid);
+            cmd1.Parameters.AddWithValue("@TotalAmount", textBox2.Text);
+            cmd1.Parameters.AddWithValue("@MiniPer", fp);
+            cmd1.Parameters.AddWithValue("@PaidAmount", textBox3.Text);
+            cmd1.Parameters.AddWithValue("@BalAmount", textBox4.Text);
+            cmd1.Parameters.AddWithValue("@PaidDate", dateTimePicker1.Value);
+            cmd1.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Stored TableFeeDetail");
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            calculatefees();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                cat = category.it_professional;
+                textBox2.Text = "3000";
+                textBox3.Text = "0";
+                textBox4.Text = "0";
+                // calculatefees();
+            }
+        }
+
+        float fp = 0;
+        float bamt = 0;
+        public void calculatefees()
+        {
+            if (cat.ToString() == "student")
+            {
+                textBox2.Text = "1000";
+
+                float ta = Convert.ToSingle(textBox2.Text);
+                fp = ta * 0.5f;
+                float amt = Convert.ToSingle(textBox3.Text);
+
+                if (amt < fp)
+                {
+                    MessageBox.Show("minimum amount to be paid is 50 per");
+                }
+                else
+                {
+                    bamt = ta - amt;
+                    textBox4.Text = bamt.ToString();
+                }
+            }
+            else if (cat.ToString() == "it_professional")
+            {
+                textBox2.Text = "3000";
+
+                float ta = Convert.ToSingle(textBox2.Text);
+                fp = ta * 0.8f;
+                float amt = Convert.ToSingle(textBox3.Text);
+
+                if (amt < fp)
+                {
+                    MessageBox.Show("minimum amount to be paid is 80 per");
+                }
+                else
+                {
+                    bamt = ta - amt;
+                    textBox4.Text = bamt.ToString();
+                }
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                cat = category.student;
+                textBox2.Text = "1000";
+                textBox3.Text = "0";
+                textBox4.Text = "0";
+                //  calculatefees();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        public void clear()
+        {
+            getnation();
+            getstate();
+            getcity();
+            radioButton1.Checked = true;
+            radioButton2.Checked = false;
+            radioButton3.Checked = true;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            dateTimePicker1.Text = DateTime.Now.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
+
---
+++ AdoDotnet/WindowsForms_Fendal_Product.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Fendal_Product
+{
+    public partial class Form1 : Form
+    {
+        string strcon = "server=LAPTOP-SUPR1U1K\\SQLEXPRESS; integrated security=true; database=ProductCategory_Fendal";
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+
+        public enum Nationality { Indian,Nri}
+        Nationality nat;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getcategory();
+            getproduct();
+            getprice();
+            nat = Nationality.Indian;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+                MessageBox.Show("Enter Only Number...");
+            }
+        }
+
+        public void getcategory()
+        {
+            con = new SqlConnection(strcon);
+            da = new SqlDataAdapter("select * from TableProductCategory", con);
+            ds = new DataSet();
+            da.Fill(ds, "tpc");
+            comboBox1.DataSource = ds.Tables["tpc"];
+            comboBox1.DisplayMember = "Product_Type_Name";
+            comboBox1.ValueMember = "Product_Category_id";
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //textBox10.Text = "0";
+            textBox11.Text = "0";
+            getproduct();
+            string catname = comboBox1.Text;
+
+            if (comboBox1.Text != "System.Data.DataRowView")
+            {                
+                con = new SqlConnection(strcon);
+                da = new SqlDataAdapter("select * from TableProductGSTDetail where GstDetail_Name=@gdn", con);
+                da.SelectCommand.Parameters.AddWithValue("@gdn", catname);
+                ds = new DataSet();
+                da.Fill(ds,"tgst");
+                foreach(DataRow dr in ds.Tables["tgst"].Rows)
+                {
+                    textBox3.Text = dr["cgst"].ToString();
+                    textBox4.Text = dr["sgst"].ToString();
+                    textBox5.Text = dr["igst"].ToString();
+                }
+            }
+        }
+
+        public void getproduct()
+        {
+            if (comboBox1.SelectedValue.ToString() != "System.Data.DataRowView")
+            {
+                con = new SqlConnection(strcon);
+                da = new SqlDataAdapter("select * from TableProduct where Product_Category_id=@Pci", con);
+                da.SelectCommand.Parameters.AddWithValue("@Pci", comboBox1.SelectedValue);
+                ds = new DataSet();
+                da.Fill(ds, "tp");
+                comboBox2.DataSource = ds.Tables["tp"];
+                comboBox2.DisplayMember = "ProductName";
+                comboBox2.ValueMember = "ProductId";
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox10.Text = "0";
+            textBox11.Text = "0";
+            getprice();
+        }
+
+        public void getprice()
+        {
+            if (comboBox2.SelectedValue.ToString() != "System.Data.DataRowView")
+            {
+                con = new SqlConnection(strcon);
+                da = new SqlDataAdapter("select * from TableProduct where ProductId=@PI", con);
+                da.SelectCommand.Parameters.AddWithValue("@PI", comboBox2.SelectedValue);
+                ds = new DataSet();
+                da.Fill(ds, "tp1");
+
+                foreach (DataRow dr in ds.Tables["tp1"].Rows)
+                {
+                    textBox9.Text = dr["ProductPrice"].ToString();
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                nat = Nationality.Indian;
+                textBox10.Text = "0";
+                textBox11.Text = "0";
+                textBox12.Text = "0";
+                textBox6.Text = "0";
+                textBox7.Text = "0";
+                textBox8.Text = "0";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(!radioButton2.Checked)
+            {
+                nat = Nationality.Nri;
+                textBox10.Text = "0";
+                textBox11.Text = "0";
+                textBox12.Text = "0";
+                textBox6.Text = "0";
+                textBox7.Text = "0";
+                textBox8.Text = "0";
+            }
+        }
+
+
+        //Quantity textbox10
+        double tcgst = 0;
+        double tsgst = 0;
+        double tigst = 0;
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox10.Text != "")
+            {
+                double price = Convert.ToDouble(textBox9.Text);
+                double quantity = Convert.ToDouble(textBox10.Text);
+                double totalamount = price * quantity;
+                textBox11.Text = totalamount.ToString();
+
+                double pcgst = Convert.ToDouble(textBox3.Text);
+                tcgst = totalamount * (pcgst / 100);
+                textBox6.Text = tcgst.ToString();
+
+                double psgst = Convert.ToDouble(textBox4.Text);
+                tsgst = totalamount * (psgst / 100);
+                textBox7.Text = tsgst.ToString();
+
+                double pigst = Convert.ToDouble(textBox5.Text);
+                tigst = totalamount * (pigst / 100);
+                textBox8.Text = tigst.ToString();
+
+                if (nat.ToString() == "Indian")
+                {
+                    double netamount = Convert.ToDouble(textBox11.Text) + Convert.ToDouble(textBox7.Text) + Convert.ToDouble(textBox8.Text);
+                    textBox12.Text = netamount.ToString();
+                }
+                else if (nat.ToString() == "Nri")
+                {
+                    double netamount = Convert.ToDouble(textBox11.Text) + Convert.ToDouble(textBox8.Text);
+                    textBox12.Text = netamount.ToString();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" && textBox2.Text == "")
+            {
+                MessageBox.Show("You can not left blank any filed...");
+            }
+            else
+            {
+                SqlConnection conn = new SqlConnection(strcon);
+                conn.Open();
+                string s1 = "insert into TableInvoiceDetails values (@Customer_Name,@Customer_Contact,@Product_Category_id,@ProductId," +
+                                                                    "@Residential_Type_Id,@Invoice_Date,@Quantity,@Price,@CGST,@SGST,@IGST," +
+                                                                    "@CGST_value,@SGST_Value,@IGST_Value,@TotalAmount)";
+
+                SqlCommand cmd = new SqlCommand(s1, conn);
+                cmd.Parameters.AddWithValue("@Customer_Name", textBox1.Text);
+                cmd.Parameters.AddWithValue("@Customer_Contact", textBox2.Text);
+                cmd.Parameters.AddWithValue("@Product_Category_id", comboBox1.SelectedValue);
+                cmd.Parameters.AddWithValue("@ProductId", comboBox2.SelectedValue);
+                cmd.Parameters.AddWithValue("@Residential_Type_Id", 1010);
+                cmd.Parameters.AddWithValue("@Invoice_Date", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@Quantity", textBox10.Text);
+                cmd.Parameters.AddWithValue("@Price", textBox9.Text);
+                cmd.Parameters.AddWithValue("@CGST", textBox3.Text);
+                cmd.Parameters.AddWithValue("@SGST", textBox4.Text);
+                cmd.Parameters.AddWithValue("@IGST", textBox5.Text);
+                cmd.Parameters.AddWithValue("@CGST_value", textBox6.Text);
+                cmd.Parameters.AddWithValue("@SGST_Value", textBox7.Text);
+                cmd.Parameters.AddWithValue("@IGST_Value", textBox8.Text);
+                cmd.Parameters.AddWithValue("@TotalAmount", textBox12.Text);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                Clear();
+                MessageBox.Show("Record inserted in TableInvoiceDetails");
+            }
+        } 
+        
+        public void Clear()
+        {
+            dateTimePicker1.Value = DateTime.Today;
+            comboBox1.Text = "---Select---";
+            comboBox2.Text = "--- Select---";
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+            textBox6.Text = "0";    
+            textBox7.Text = "0";
+            textBox8.Text = "0";
+            textBox9.Text = "0";
+            textBox10.Text = "0";
+            textBox11.Text = "0";
+            textBox12.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+    }
+}
+    
+
