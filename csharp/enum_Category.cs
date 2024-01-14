c741571dbfccd2e81d55466c5a01d7a542387fb4
aayushitale280701 <ayushitale2020@gmail.com>
2024-01-14 19:51:20 +0530

    enum_Category.cs         enum_csharp.cs

---
+++ csharp/enum_Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace enum_Category
+{
+    internal class Program
+    {
+        enum category { Student, IT_Professional};
+        static void Main(string[] args)
+        {
+            int c = Convert.ToInt32(category.Student);
+            if (c == 0)
+            {
+                Console.WriteLine("Category is Student");
+            }
+            else if (c == 1)
+            {
+                Console.WriteLine("Category is IT_Professional");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/enum_csharp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace enum_csharp
+{
+    class Program
+    {
+        enum grade { a,b, c };
+        static void Main(string[] args)
+        {
+            int g = Convert.ToInt32(grade.b);
+            Console.WriteLine("Value of b is..." + g);
+            Console.ReadLine();
+        }
+    }
+}
